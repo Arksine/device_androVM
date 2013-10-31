6b908dcf2a9b7009bfbfcbffa000e0b394c5b09b
Sylvain Galand <sgaland@genymobile.com>
2013-10-31 19:41:43 +0100

    [US116.2] Add functions to handle check and flash or archives
    Executing scripts using system

---
+++ common/system/genyd/archive_flash_handler.cpp
@@ -0,0 +1,55 @@
+
+#include <cutils/properties.h>
+
+#include <sstream>
+#include <string>
+
+#include "dispatcher.hpp"
+#include "libgenyd.hpp"
+
+#define CHECK_ARCHIVE_SCRIPT "/system/bin/check-archive.sh "
+#define FLASH_ARCHIVE_SCRIPT "/system/bin/flash-archive.sh "
+
+void Dispatcher::checkArchive(const Request &request, Reply *reply)
+{
+    std::string archive_path = request.parameter().value().stringvalue();
+    
+    SLOGD("Received CheckArchive %s", archive_path.c_str());
+
+    std::string script = CHECK_ARCHIVE_SCRIPT;
+    std::string cmd = script + archive_path;
+
+    Status *status = reply->mutable_status();
+
+    int res = system(cmd.c_str());
+
+    SLOGD("CheckArchive : %s - %d", cmd.c_str(), res);
+
+    if (res == 0) {
+        status->set_code(Status::Ok);
+    } else {
+        status->set_code(Status::GenericError);
+    }
+}
+
+void Dispatcher::flashArchive(const Request &request, Reply *reply)
+{
+    std::string archive_path = request.parameter().value().stringvalue();
+
+    SLOGD("Received FlashArchive %s", archive_path.c_str());
+
+    std::string script = FLASH_ARCHIVE_SCRIPT;
+    std::string cmd = script + archive_path;
+
+    Status *status = reply->mutable_status();
+
+    int res = system(cmd.c_str());
+
+    SLOGD("FlashArchive : %s - %d", cmd.c_str(), res);
+
+    if (res == 0) {
+        status->set_code(Status::Ok);
+    } else {
+        status->set_code(Status::GenericError);
+    }
+}
