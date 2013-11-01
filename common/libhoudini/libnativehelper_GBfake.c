7ab8771c59542a6022965ee55b62c475e284e243
Daniel Fages <dan@androvm.org>
2013-11-01 13:11:19 +0100

    Remove these files

--- common/libhoudini/libnativehelper_GBfake.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "cutils/log.h"
-
-#define LOG_TAG "libnativehelper_GBfake"
-
-int jniRegisterSystemMethods(void *p) {
-    ALOGE("fake jniRegisterSystemMethods() called");
-    return 1;
-}
