54fdefc01b3d9d3febec4e6e3162c63a5d34ea6c
Samuel Rats <srats@genymobile.com>
2013-12-30 12:54:25 +0100

    [US225] Missing files

---
+++ common/services/clipboard/src/com/genymotion/clipboardproxy/StartAtBootClipboardProxy.java
@@ -0,0 +1,20 @@
+package com.genymotion.clipboardproxy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StartAtBootClipboardProxy extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			Intent serviceIntent = new Intent(context, ServiceClipboardProxy.class);
+
+			Log.d("clipboardproxy", "StartAtBootClipBoardProxy");
+
+			context.startService(serviceIntent);
+		}
+	}
+}
