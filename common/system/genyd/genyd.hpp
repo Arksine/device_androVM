5ae8c1036a31515a3a17de9f07f0f7aef834222c
Samuel Rats <srats@genymobile.com>
2014-01-02 11:47:02 +0100

    [US225] Treate SetClipboard as a "standard" command

--- common/system/genyd/genyd.hpp
+++ common/system/genyd/genyd.hpp
@@ -11,46 +11,55 @@
 
 #define SERVER_PORT 22666
 
-class Genyd {
+class Genyd
+{
 
 public:
-  Genyd(void);
-  ~Genyd(void);
+    Genyd(void);
+    ~Genyd(void);
 
 private:
-  Genyd(const Genyd &);
-  Genyd operator=(const Genyd &);
+    Genyd(const Genyd &);
+    Genyd operator=(const Genyd &);
 
 private:
-  Socket *server;
-  Dispatcher dispatcher;
-  std::map<int, Socket *> clients;
+    Socket *server;
+    Dispatcher dispatcher;
 
-  // socket to the clipboardProxy service
-  // this socket is included in clients too
-  Socket* clipboardProxy;
+    // Clients list
+    std::map<int, Socket *> clients;
 
-  // Keep clipboard content
-  std::string clipboard;
+    // socket to the clipboardProxy service
+    // this socket is also present in the clients list
+    Socket *clipboardProxy;
 
-  // Initialize fd_set for select() monitoring
-  int setFS(fd_set *readfs, fd_set *writefs) const;
+    // Keep clipboard content
+    std::string clipboard;
 
-  // Accept a new connection
-  void acceptNewClient(void);
+    // Initialize fd_set for select() monitoring
+    int setFS(fd_set *readfs, fd_set *writefs) const;
 
-  // Handle Socket::read status for a given client
-  void treatMessage(Socket *client);
+    // Accept a new connection
+    void acceptNewClient(void);
 
-  void sendHostClipboardToAndroid(const Request &request);
-  void sendAndroidClipboardToHost();
+    // Handle Socket::read status for a given client
+    void treatMessage(Socket *client);
+
+    void sendHostClipboardToAndroid(const Request &request);
+    void sendAndroidClipboardToHost();
 
 public:
-  // Start server
-  void run(void);
+    // Start server
+    void run(void);
 
-  // Check if the server is well initialized
-  bool isInit(void) const;
+    // Check if the server is well initialized
+    bool isInit(void) const;
+
+    // Retreive the connection to the ClipboardService
+    Socket *getClipboardClient(void);
+
+    // Store the clipboard
+    void storeClipboard(const std::string &clipboard);
 
 };
 
